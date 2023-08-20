1f95dcb84f5b28bfed76571dec1f007a76e5d224
HromayaHorse <the_pigeon_is_eating@protonmail.com>
2023-08-20 22:24:42 +0300

    fix appjs

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const { Telegraf } = require('telegraf');
 const rateLimit = require('telegraf-ratelimit')
 
-const limitConfig = {
-  window: 3000,
-  limit: 1,
-  onLimitExceeded: (ctx, next) => ctx.reply(`Не произноси имени Господа, Бога твоего, напрасно; 
-  ибо Господь не оставит без наказания того, кто произносит имя Его напрасно.`)
-}
-
 const bot = new Telegraf(token);
 
 const Api = require('./api.js');
@@ -15,7 +8,6 @@ const api = new Api();
 
 const schedule = require('node-schedule');
 
-bot.use(rateLimit(limitConfig))
 
 bot.command('start', async ctx => {
     await bot.telegram.sendPhoto(ctx.chat.id, "https://i.pinimg.com/736x/cc/79/af/cc79af14c1de90ded80526e18d4e0975--savior-jesus-christ.jpg")
@@ -34,11 +26,11 @@ bot.command('start', async ctx => {
             ]
           }
     })
-    const job = schedule.scheduleJob('0 0 0 * * *', async() => {
-      let quote = await api.getJesusQuote();
-      await bot.telegram.sendPhoto(ctx.chat.id, quote.data[0].link)
-      await bot.telegram.sendMessage(ctx.chat.id, JSON.stringify(quote.data[0].quote))
-    });
+const job = schedule.scheduleJob('0 0 0 * * *', async() => {
+    let quote = await api.getJesusQuote();
+    await bot.telegram.sendPhoto(ctx.chat.id, quote.data[0].link)
+    await bot.telegram.sendMessage(ctx.chat.id, JSON.stringify(quote.data[0].quote))
+  });
 });
 
 bot.hears("Получить мудрость", async (ctx) => {
@@ -52,5 +44,14 @@ bot.hears("Просто посидеть вместе с Иисусом", async 
   await bot.telegram.sendPhoto(ctx.chat.id, pic.data[0].link)
 })
 
+const limitConfig = {
+  window: 3000,
+  limit: 1,
+  onLimitExceeded: (ctx, next) => ctx.reply(`Не произноси имени Господа, Бога твоего, напрасно; 
+  ибо Господь не оставит без наказания того, кто произносит имя Его напрасно.`)
+}
+
+bot.use(rateLimit(limitConfig))
+
 bot.launch();
-console.log("Bot started")
+console.log("Jesus Bot started")
